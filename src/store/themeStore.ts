88965a676307e635b7a86b43cd0e1e82b49edb95
Max Kim <hwaseen@gmail.com>
2024-02-05 22:49:53 +0900

    fix:빌드에러 수정

--- src/store/themeStore.ts
+++ src/store/themeStore.ts
@@ -4,13 +4,20 @@ import { Theme } from '../types';
 export const STORAGE_KEY_THEME = 'maxkim-blog-theme';
 export const THEME_LIST: Theme[] = ['light', 'dark'];
 
+const getThemeFromBrowser = () => {
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem(STORAGE_KEY_THEME) ?? 'light';
+  }
+  return null;
+};
+
 interface ThemeStore {
   theme: Theme;
   setTheme: (theme: Theme) => void;
 }
 
 export const useThemeStore = create<ThemeStore>((set) => ({
-  theme: (localStorage.getItem(STORAGE_KEY_THEME) ?? 'light') as Theme,
+  theme: (getThemeFromBrowser() ?? 'light') as Theme,
   setTheme: (theme: Theme) => {
     if (typeof window !== 'undefined') {
       window.localStorage.setItem(STORAGE_KEY_THEME, theme);
